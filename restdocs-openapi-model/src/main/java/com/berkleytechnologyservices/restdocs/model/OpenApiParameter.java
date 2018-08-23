d8cc74b38cd71dd86ec158653c4fe927a78196ee
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2018-08-23 12:32:21 -0500

    feat(*): add model project

---
+++ restdocs-openapi-model/src/main/java/com/berkleytechnologyservices/restdocs/model/OpenApiParameter.java
@@ -0,0 +1,19 @@
+package com.berkleytechnologyservices.restdocs.model;
+
+public class OpenApiParameter {
+
+  private String name;
+
+  public OpenApiParameter(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
