06e41202a519ad74eaa91a4f9b292ecaaf5907a8
cnsgithub <cnsgithub@users.noreply.github.com>
2018-12-06 09:16:28 +0100

    add support for generating OpenAPI 3.0 specifications

---
+++ restdocs-spec-generator/src/test/java/com/berkleytechnologyservices/restdocs/spec/generator/openapi_v3/OpenApi30SpecificationGeneratorTest.java
@@ -0,0 +1,73 @@
+package com.berkleytechnologyservices.restdocs.spec.generator.openapi_v3;
+
+import com.berkleytechnologyservices.restdocs.spec.ApiDetails;
+import com.berkleytechnologyservices.restdocs.spec.Specification;
+import com.berkleytechnologyservices.restdocs.spec.generator.openapi_v2.OpenApi20SpecificationGenerator;
+import com.epages.restdocs.apispec.model.HTTPMethod;
+import com.epages.restdocs.apispec.model.ResourceModel;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.berkleytechnologyservices.restdocs.spec.generator.test.ResourceModels.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.contentOf;
+import static org.assertj.core.util.Lists.emptyList;
+import static org.assertj.core.util.Lists.list;
+
+@ExtendWith(MockitoExtension.class)
+public class OpenApi30SpecificationGeneratorTest {
+
+  private final OpenApi30SpecificationGenerator generator = new OpenApi30SpecificationGenerator();
+
+  @Test
+  public void testGetSpecification() {
+    assertThat(generator.getSpecification()).isEqualTo(Specification.OPENAPI_V3);
+  }
+
+  @Test
+  public void testGenerateWithDefaults() {
+
+    ApiDetails apiDetails = new ApiDetails();
+
+    ResourceModel model = resource(
+            "book-get",
+            "Get a book by id",
+            "book",
+            request(
+                    "/book/{id}",
+                    HTTPMethod.GET,
+                    list(
+                            requiredParam("id", "The unique identifier for the book.", "NUMBER")
+                    ),
+                    emptyList()
+            ),
+            response(
+                    200,
+                    "application/hal+json",
+                    list(),
+                    list(
+                            field("title", "Title of the book", "STRING"),
+                            field("author", "Author of the book", "STRING"),
+                            field("pages", "Number of pages in the book", "NUMBER")
+                    ),
+                    "The example response.",
+                    "type: string"
+
+            )
+    );
+
+    String rawOutput = generator.generate(apiDetails, list(model));
+
+    assertThat(rawOutput)
+            .isEqualToNormalizingNewlines(contentOfResource("/mock-specs/default-settings-openapi3.yml"));
+  }
+
+  private static String contentOfResource(String resourceName) {
+    return contentOf(OpenApi30SpecificationGeneratorTest.class.getResource(resourceName));
+  }
+}
