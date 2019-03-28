17007fa34c6880546e6e9740ffa31074093f04c6
Kyle Lieber <klieber@users.noreply.github.com>
2019-03-28 13:38:06 -0500

    chore: release script updates

--- release.sh
+++ release.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-releaseVersion=$1
-nextVersion=$2
-
-echo "creating release $releaseVersion" \
-  && mvn versions:set -DnewVersion=$releaseVersion \
+echo "creating release" \
+  && mvn -q build-helper:parse-version \
+            versions:set -DnewVersion=0.\${parsedVersion.minorVersion} \
+            versions:commit \
+  && releaseVersion=$(mvn -q build-helper:parse-version help:evaluate -Dexpression=project.version -DforceStdout) \
   && git add pom.xml */pom.xml \
   && git commit -m "release $releaseVersion" \
   && git tag -a $releaseVersion -m "release $releaseVersion" \
-  && echo "preparing for next release $nextVersion" \
-  && mvn versions:set -DnewVersion=$nextVersion-SNAPSHOT \
+  && mvn -l release.log clean deploy \
+  && echo "release created: $releaseVersion" \
+  && echo "preparing for next release" \
+  && mvn -q build-helper:parse-version \
+            versions:set -DnewVersion=0.\${parsedVersion.nextMinorVersion}-SNAPSHOT \
+            versions:commit \
+  && nextVersion=$(mvn -q build-helper:parse-version help:evaluate -Dexpression=project.version -DforceStdout | sed 's/-SNAPSHOT//') \
   && git add pom.xml */pom.xml \
   && git commit -m "prepare for development of $nextVersion" \
+  && echo "ready for next release: $nextVersion" \
   && echo "pushing changes" \
   && git push upstream master:master \
   && git push upstream $releaseVersion:$releaseVersion
