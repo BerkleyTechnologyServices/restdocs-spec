b00857fc751a79d84c437736f0434c58b5ffcdb0
Kyle Lieber <klieber@users.noreply.github.com>
2019-09-23 11:58:19 -0500

    chore(release): use maven wrapper

--- release.sh
+++ release.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 echo "creating release" \
-  && mvn -q build-helper:parse-version \
+  && ./mvnw -q build-helper:parse-version \
             versions:set -DnewVersion=0.\${parsedVersion.minorVersion} \
             versions:commit \
-  && releaseVersion=$(mvn -q build-helper:parse-version help:evaluate -Dexpression=project.version -DforceStdout) \
+  && releaseVersion=$(./mvnw -q build-helper:parse-version help:evaluate -Dexpression=project.version -DforceStdout) \
   && git add pom.xml */pom.xml \
   && git commit -m "release $releaseVersion" \
   && git tag -a $releaseVersion -m "release $releaseVersion" \
-  && mvn -l release.log clean deploy \
+  && ./mvnw -l release.log clean deploy \
   && echo "release created: $releaseVersion" \
   && echo "preparing for next release" \
-  && mvn -q build-helper:parse-version \
+  && ./mvnw -q build-helper:parse-version \
             versions:set -DnewVersion=0.\${parsedVersion.nextMinorVersion}-SNAPSHOT \
             versions:commit \
-  && nextVersion=$(mvn -q build-helper:parse-version help:evaluate -Dexpression=project.version -DforceStdout | sed 's/-SNAPSHOT//') \
+  && nextVersion=$(./mvnw -q build-helper:parse-version help:evaluate -Dexpression=project.version -DforceStdout | sed 's/-SNAPSHOT//') \
   && git add pom.xml */pom.xml \
   && git commit -m "prepare for development of $nextVersion" \
   && echo "ready for next release: $nextVersion" \
